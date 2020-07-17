80514e999ac64152f976bad1b82da192ccf5df6e
blackowe <blackowe@oregonstate.edu>
2020-07-17 07:08:40 -0500

    Updated commit

--- multiply.py
+++ multiply.py
@@ -1,11 +1,12 @@
-#Author: Erik Blackowicz
+#Author:Erik Blackowicz
 #Date: 7/15/20
-#Description: Creates multiply fx using recursive addition
-# Recursive addition - reduces 'a' by 1 until reaches 0, then returns to base case to add 'b' value 'a' amount of times.
+#Description: Project 5a. Creates multiply fx (product of 2 integers) by using recursive addition.
+# Does not work if both integers are negative. Recursive addition action - reduces 'num1' condition by 1 until reaches 0,
+# then returns to the base case to add 'num2' value 'num1' amount of times.
 
-def multiply(a,b):
-  ''' takes in two positive integer parameters and multiplies them using recursive addition.'''
-  if a == 0 or b == 0: # base case
+def multiply(num1,num2): # num2 can be a negative value, num1 must be positive value
+  ''' Takes in two positive integer parameters and multiplies them using recursive addition. Num2 can be a negative value'''
+  if num1 == 0 or num2 == 0: # base case
     return 0
   else: # starts recursive case
-    return b + multiply(a-1, b)
+    return num2 + multiply(num1-1, num2)
